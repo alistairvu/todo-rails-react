e9931ee702061e56ce261b328ae1ca8dfee805df
Alistair Vu <alistairvu@gmail.com>
2021-03-11 23:43:50 +0700

    Add frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import Container from "react-bootstrap/Container"
+
+const App = () => {
+  return (
+    <main>
+      <Container className="text-center py-5">
+        <h1>Welcome to React!</h1>
+        <h3 style={{ fontSize: 30 }}>
+          Get started by editing{" "}
+          <code style={{ color: "red" }}>src/App.tsx</code>
+        </h3>
+      </Container>
+    </main>
+  )
+}
+
+export default App
---
+++ frontend/src/index.tsx
@@ -0,0 +1,6 @@
+import { render } from "react-dom"
+import App from "./App"
+import "./bootstrap.min.css"
+
+const rootElement = document.getElementById("root")
+render(<App />, rootElement)
